350062864d49d4fb6da9081d4d0688dde6719226
Ahamed Aaqib <ahamedaaqib96@outlook.com>
2020-04-22 20:43:17 +0530

    Initial Commit

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,16 +1,44 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace GradeBook
 {
-    class Book
+    public class Book
     {
-       // List<double> grades;
-        
-       /* public void AddGrade(double grade)
+        private List<double> grades ;
+        private string name;
+
+        public Book(string name)
         {
-           grades.Add(grade);
+            grades = new List<double>();
+            this.name = name;
         }
-        */
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            double result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade  = double.MaxValue;
+
+            foreach (var number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+
+            result /= grades.Count;
+
+            Console.WriteLine($"The Highest Grade is {highGrade}");
+            Console.WriteLine($"The Lowest Grade is {lowGrade}");
+            Console.WriteLine($"The Average Grade is {result:N3}");
+
+        }
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,28 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 
 namespace GradeBook
 {
     class Program
-    {       
+    {
         static void Main(string[] args)
         {
-            var book = new Book();
-          //  book.AddGrade(89.1);
+            var book = new Book("Aaqib's GradeBook");
+            book.AddGrade(84.5);
+            book.AddGrade(75.4);
+            book.AddGrade(96.2);
+            book.ShowStatistics();
 
-           var grades = new List<double>(){ 12.7,10.3,6.11,4.1 };
-           grades.Add(56.1);
 
-           var result =0.0;
-
-           foreach(var number in grades)
-           {
-               result +=number;
-           }
            
-           result /=grades.Count;
 
-           Console.WriteLine($"The average grade is {result:N3}");
 
 
         }
