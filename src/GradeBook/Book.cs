ad9fbe3795ca759355252a1282ddeac9f39d3f48
Ahamed Aaqib <ahamedaaqib96@outlook.com>
2020-04-26 14:36:40 +0530

    Character Base Resullt Added -Switch

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -6,7 +6,7 @@ namespace GradeBook
 {
     public class Book
     {
-        private List<double> grades ;
+        private List<double> grades;
         public string Name;
 
         public Book(string name)
@@ -15,18 +15,51 @@ namespace GradeBook
             Name = name;
         }
 
-      
+        public void AddLetterGrade(char letter)
+        {
+            switch (letter)
+            {
+                case 'A':
+                    AddGrade(75);
+                    break;
+                case 'B':
+                    AddGrade(65);
+                    break;
+                case 'C':
+                    AddGrade(50);
+                    break;
+                case 'D':
+                    AddGrade(40);
+                    break;
+                default:
+                    AddGrade(0);
+                    break;
+
+            }
+
+        }
+
+
         public void AddGrade(double grade)
         {
-            grades.Add(grade);
+            if (grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid Grade !");
+            }
+
         }
 
         public Statistics GetStatistics()
         {
             var result = new Statistics();
-            result.Average =0.0;
+            result.Average = 0.0;
             result.High = double.MinValue;
-            result.Low  = double.MaxValue;
+            result.Low = double.MaxValue;
 
             foreach (var grade in grades)
             {
@@ -37,7 +70,27 @@ namespace GradeBook
 
             result.Average /= grades.Count;
 
-           return result;
+            switch (result.Average)
+            {
+                case var d when d >= 75:
+                    result.Letter = 'A';
+                    break;
+                case var d when d >= 65:
+                    result.Letter = 'B';
+                    break;
+                case var d when d >= 50:
+                    result.Letter = 'C';
+                    break;
+                case var d when d >= 40:
+                    result.Letter = 'A';
+                    break;
+                default:
+                    result.Letter = 'F';
+                    break;
+
+            }
+
+            return result;
 
         }
     }
--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -9,25 +9,24 @@ namespace GradeBook.Tests
         public void CalaculateAnAverageGrade()
         {
             //arrange
-           var book = new Book("");
+            var book = new Book("");
 
-           book.AddGrade(89.1);
-           book.AddGrade(90.5);
-           book.AddGrade(77.3);
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
+            book.AddGrade(85.3);//without this value also same average will come -maths magic 
+            book.AddGrade(1001.1);//not going to include in average calaulation
 
-           //actual
-           var result = book.GetStatistics();
+            //actual
+            var result = book.GetStatistics();
 
 
             //Assert
 
-            Assert.Equal(85.6, result.Average,1);//getting average in 1 decimal place 
+            Assert.Equal(85.6, result.Average, 1);//getting average in 1 decimal place 
             Assert.Equal(90.5, result.High);
             Assert.Equal(77.3, result.Low);
-
-           
-            
-
+            Assert.Equal('A', result.Letter);
 
 
         }
